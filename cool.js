a808037e7fd051c5ac596555a859d8e3d44f639b
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2020-09-01 16:43:50 -0700

    use JS to only target the run button

--- cool.js
+++ cool.js
@@ -1,3 +1,14 @@
 for (var row of document.querySelectorAll(".Box-row > div > div > span > a")) {
   row.classList.add("insta-gradient");
 }
+
+// Rainbow the workflow run button
+document.querySelectorAll('include-fragment').forEach(item => {
+  item.addEventListener('load', event => {
+    var runButtonXPath = "//button[@type='submit' and contains(., 'Run workflow')]";
+    var runButton = document.evaluate(runButtonXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (runButton) {
+      runButton.classList.add("btn-rainbow")
+    }
+  })
+})
